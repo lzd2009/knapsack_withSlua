c5a466a81af3b55fdfa52f28a6a28bf3d0fb12c4
linzz <1073958028@qq.com>
2020-01-03 18:27:47 +0800

    first commit

---
+++ Assets/Scripts/LuaFileManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+using System;
+using UnityEngine.UI;
+[CustomLuaClass]
+public class LuaFileManager : MonoBehaviour {
+
+    public LuaSvr luaSvr;
+
+    public LuaTable self;
+
+    public delegate void LifeFunctionDelegate();
+
+    public LifeFunctionDelegate d_start;
+    public LifeFunctionDelegate d_update;
+    public LifeFunctionDelegate d_onEnable;
+    public LifeFunctionDelegate d_onDisable;
+    
+    public void TestArea()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        luaSvr = new LuaSvr();
+        luaSvr.init(null, OnComplete);
+
+        if (d_start != null)
+            d_start();
+
+    }
+	private void OnComplete()
+    {
+        self = (LuaTable)luaSvr.start("luaFile/knapsack");
+        LuaFunction luafunc_start = (LuaFunction)self["Start"];
+        LuaFunction luafunc_update = (LuaFunction)self["Update"];
+        LuaFunction luafunc_onEnable = (LuaFunction)self["OnEnable"];
+        LuaFunction luafunc_onDisable = (LuaFunction)self["OnDisable"];
+
+        d_start = luafunc_start.cast<LifeFunctionDelegate>();
+        d_update = luafunc_update.cast<LifeFunctionDelegate>();
+        d_onEnable = luafunc_onEnable.cast<LifeFunctionDelegate>();
+        d_onDisable = luafunc_onDisable.cast<LifeFunctionDelegate>();
+    }
+
+    private void Update()
+    {
+        if (d_update != null)
+            d_update();
+    }
+
+    private void OnEnable()
+    {
+        if (d_onEnable != null)
+            d_onEnable();
+    }
+
+    private void OnDisable()
+    {
+        if (d_onDisable != null)
+            d_onDisable();
+    }
+
+    public static Sprite LoadSprite(string path)
+    {
+        return Resources.Load<Sprite>(path);
+    }
+}
---
+++ Assets/Slua/LuaObject/Custom/Lua_LuaFileManager.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using SLua;
+using System.Collections.Generic;
+[UnityEngine.Scripting.Preserve]
+public class Lua_LuaFileManager : LuaObject {
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int TestArea(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			self.TestArea();
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int LoadSprite_s(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			System.String a1;
+			checkType(l,1,out a1);
+			var ret=LuaFileManager.LoadSprite(a1);
+			pushValue(l,true);
+			pushValue(l,ret);
+			return 2;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int get_luaSvr(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			pushValue(l,true);
+			pushValue(l,self.luaSvr);
+			return 2;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_luaSvr(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			SLua.LuaSvr v;
+			checkType(l,2,out v);
+			self.luaSvr=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int get_self(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			pushValue(l,true);
+			pushValue(l,self.self);
+			return 2;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_self(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			SLua.LuaTable v;
+			checkType(l,2,out v);
+			self.self=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_d_start(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			LuaFileManager.LifeFunctionDelegate v;
+			int op=checkDelegate(l,2,out v);
+			if(op==0) self.d_start=v;
+			else if(op==1) self.d_start+=v;
+			else if(op==2) self.d_start-=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_d_update(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			LuaFileManager.LifeFunctionDelegate v;
+			int op=checkDelegate(l,2,out v);
+			if(op==0) self.d_update=v;
+			else if(op==1) self.d_update+=v;
+			else if(op==2) self.d_update-=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_d_onEnable(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			LuaFileManager.LifeFunctionDelegate v;
+			int op=checkDelegate(l,2,out v);
+			if(op==0) self.d_onEnable=v;
+			else if(op==1) self.d_onEnable+=v;
+			else if(op==2) self.d_onEnable-=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[SLua.MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	[UnityEngine.Scripting.Preserve]
+	static public int set_d_onDisable(IntPtr l) {
+		try {
+			#if DEBUG
+			var method = System.Reflection.MethodBase.GetCurrentMethod();
+			string methodName = GetMethodName(method);
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.BeginSample(methodName);
+			#else
+			Profiler.BeginSample(methodName);
+			#endif
+			#endif
+			LuaFileManager self=(LuaFileManager)checkSelf(l);
+			LuaFileManager.LifeFunctionDelegate v;
+			int op=checkDelegate(l,2,out v);
+			if(op==0) self.d_onDisable=v;
+			else if(op==1) self.d_onDisable+=v;
+			else if(op==2) self.d_onDisable-=v;
+			pushValue(l,true);
+			return 1;
+		}
+		catch(Exception e) {
+			return error(l,e);
+		}
+		#if DEBUG
+		finally {
+			#if UNITY_5_5_OR_NEWER
+			UnityEngine.Profiling.Profiler.EndSample();
+			#else
+			Profiler.EndSample();
+			#endif
+		}
+		#endif
+	}
+	[UnityEngine.Scripting.Preserve]
+	static public void reg(IntPtr l) {
+		getTypeTable(l,"LuaFileManager");
+		addMember(l,TestArea);
+		addMember(l,LoadSprite_s);
+		addMember(l,"luaSvr",get_luaSvr,set_luaSvr,true);
+		addMember(l,"self",get_self,set_self,true);
+		addMember(l,"d_start",null,set_d_start,true);
+		addMember(l,"d_update",null,set_d_update,true);
+		addMember(l,"d_onEnable",null,set_d_onEnable,true);
+		addMember(l,"d_onDisable",null,set_d_onDisable,true);
+		createTypeMetatable(l,null, typeof(LuaFileManager),typeof(UnityEngine.MonoBehaviour));
+	}
+}
